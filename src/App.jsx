9cea2a8d9f3b302e2c913ec1250a6e67bbaf9e4e
Lucie CORREIA <lucie.correia@telecomnancy.eu>
2024-08-05 02:24:40 +0200

    init + jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-zinc-900 min-h-screen flex flex-col">
+      <div className="px-4 py-4 border-b flex">
+        <h1 className="w-full text-slate-50 text-4xl">IDEx</h1>
+        <div className="w-full">*
+          <button>Montrer la preview</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
